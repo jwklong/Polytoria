2d42e44905bc6e5e25a58e932534fc444fa849b3
jwklong <jwklong@gmx.com>
2026-05-26 21:42:22 +0100

    oops
    Signed-off-by: jwklong <jwklong@gmx.com>

--- Polytoria/scripts/datamodel/resources/ResourceAsset.cs
+++ Polytoria/scripts/datamodel/resources/ResourceAsset.cs
@@ -22,7 +22,7 @@ public partial class ResourceAsset : BaseAsset
 	public PTSignal ResourceLoadedInternal { get; private set; } = new();
 	
 	[ScriptProperty]
-	public bool Loading => !IsResourceLoading;
+	public bool Loading => !IsResourceLoaded;
 	
 	[ScriptProperty] public PTSignal Loaded { get; private set; } = new();
 
